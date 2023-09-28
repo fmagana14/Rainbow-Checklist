f0779c570b1c286a47b28f98568e3f04b8f9e099
fmagana14 <142852197+fmagana14@users.noreply.github.com>
2023-09-27 20:08:05 -0700

    created CRUD function

---
+++ checklist.py
@@ -0,0 +1,22 @@
+checklist= list()
+    #create
+def create(item):
+    checklist.append('item')
+
+    #read
+def read (index) :
+    return checklist[index]
+
+    #update
+
+def update (index, item):
+    checklist [index] = item
+
+    #distroy
+
+def destroy(index):
+    checklist.pop(index)
+
+def test():
+    print ('Purple Sox')
+    test()
